a23cd6828a5631ae664b8a8a36395ea374b99a33
Rémi ARNAUD <remiarnaud.pro@gmail.com>
2017-10-25 15:04:04 +0200

    feat: add a created date to items

--- js/dothatthing.js
+++ js/dothatthing.js
@@ -138,7 +138,7 @@ App.task_manager = {
     var value = item.val();
     var list_id = this.get_item_id(item.closest('.list'));
 
-    _.filter(App.todolists, {id: list_id})[0].items.push({id: id, value: value, created_at: moment()})
+    _.filter(App.todolists, {id: list_id})[0].items.push({id: id, value: value, created_at: moment().format()})
   },
 
   add_todo_item: function(btn) {
